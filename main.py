20511d8effef20c1b0c065536bafa82d3ad0d58f
DavidKovalov92 <kovalovdavid92@gmail.com>
2025-05-08 15:58:45 +0000

    final final

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from api_v1.users.views import router as users_router
 from api_v1.expenses.views import router as expenses_router
-from api_v1.jwt_auth.views import router as demo_auth_router
 from api_v1.jwt_auth.jwt_auth import router as jwt_auth_router
 
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(expenses_router)
-app.include_router(demo_auth_router)
 app.include_router(jwt_auth_router)
 
 
